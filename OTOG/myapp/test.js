d48d7a17818ff4c26c0204ce2a30fc8e58903ee0
karnjj <karnjj@hotmail.com>
2019-05-29 19:38:40 -0700

    Add fetching function.

--- OTOG/myapp/test.js
+++ OTOG/myapp/test.js
@@ -1,6 +1,8 @@
 const exec = require("child_process").execSync;
 exec("g++ -std=c++11 -O2 gcctest.cpp -o gcctest.out");
+console.log('compile complete!!');
 var result = exec("./gcctest.out");
+exec("rm gcctest.out");
 
 // convert and show the output.
 console.log(result.toString("utf8"));
