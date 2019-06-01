bb4fc9af90f33811d30ce273a2980e6446d617ec
Amethyst <39304441+SctPimming@users.noreply.github.com>
2019-06-01 11:41:42 +0700

    Update test.js

--- OTOG/myapp/test.js
+++ OTOG/myapp/test.js
@@ -1,6 +1,7 @@
 const exec = require("child_process").execSync;
-exec("g++ -std=c++11 -O2 gcctest.cpp -o gcctest.out");
-var result = exec("./gcctest.out");
+
+exec("g++ -std=c++11 -O2 gcctest.cpp -o exp_output.out");
+var result = exec("./exp_output.out");
 
 // convert and show the output.
 console.log(result.toString("utf8"));
