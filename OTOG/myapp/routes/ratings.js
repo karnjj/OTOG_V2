25121da7bba0f14531e9d9c25aa0211247691fa5
karnjj <karnjj@hotmail.com>
2019-06-12 01:17:21 -0700

    fix rating color

--- OTOG/myapp/routes/ratings.js
+++ OTOG/myapp/routes/ratings.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "0000",
+  database: "OTOG"
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render("ratings.html", { title: 'ratings' });
+	var sql = "SELECT * FROM User";
+	con.query(sql, function (err, rows) {
+    	if (err) throw err;
+    	//console.log(rows);
+		res.render("ratings.html", {
+			title: 'ratings',
+			users : rows,
+		});
+	});	
+  	//res.render("ratings.html", { title: 'ratings' });
 });
+
+router.get('/head_rating', function(req, res, next) {
+	res.render("head_rating.html");
+})
 
 module.exports = router;
