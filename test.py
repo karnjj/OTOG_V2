04b39783ff37d85dee794ce04d60e52575d164b7
karnjj <karnjj@hotmail.com>
2019-06-22 21:07:46 -0700

    fix compile

--- test.py
+++ test.py
@@ -61,7 +61,36 @@ def execute(language, username, filename, testcase, timelimit, memlimit):
 	os.system("pkill "+ exename)
 	print("Return Code : "+str(t))
 	return t
+def cmpfunc(fname1,fname2) :
+	# Open file for reading in text mode (default mode)
+	f1 = open(fname1)
+	f2 = open(fname2)
 
+	# Read the first line from the files
+	f1_line = f1.readline()
+	f2_line = f2.readline()
+
+	# Loop if either file1 or file2 has not reached EOF
+	while f1_line != '' or f2_line != '':
+
+	    # Strip the leading whitespaces
+	    f1_line = f1_line.rstrip()
+	    f2_line = f2_line.rstrip()
+	    
+	    # Compare the lines from both file
+	    if f1_line != f2_line:
+	    	f1.close()
+	    	f2.close()
+	    	return False
+
+	    #Read the next line from the file
+	    f1_line = f1.readline()
+	    f2_line = f2.readline()
+
+	# Close the files
+	f1.close()
+	f2.close()
+	return True
 while 1 :
 	mycursor = mydb.cursor()
 	mycursor.execute("SELECT * FROM Result WHERE status = 0 ORDER BY time")
@@ -83,8 +112,8 @@ while 1 :
 		if(result==None) :
 			for x in range(10):
 				t = execute("C++", user_name, file_name,str(x+1), time_limit, mem_limit)
-				result_user = file_read("env/output.txt")
-				result_src = file_read("source/"+file_name+"/"+str(x+1)+".sol")
+				result_user = "env/output.txt"
+				result_src = "source/"+file_name+"/"+str(x+1)+".sol"
 				timetaken = 0
 				if t == 124:
 					result = "TLE"
@@ -107,16 +136,18 @@ while 1 :
 					timetaken = timediff
 				sumtime = sumtime + timetaken
 				if(result == None and t == 0) :
-					if(result_user == result_src): 
+					if(cmpfunc(result_user,result_src)): 
 						ans = ans + 'P'
 						cnt = cnt + 1
 					else : ans = ans + '-' 
 				elif(result == 'TLE') :
 					ans = ans + 'T'
 				else: ans = ans + 'X'
-			print(ans)
-			print("TIME : " + str(sumtime))
-			score = (cnt/10)*100
+		else :
+			ans = result
+		print(ans)
+		print("TIME : " + str(sumtime))
+		score = (cnt/10)*100
 		sql = "UPDATE Result SET result = %s, score = %s, timeuse = %s, status = 1 WHERE idResult = %s"
 		val = (ans, score, sumtime, myresult[0])
 		mycursor.execute(sql, val)
