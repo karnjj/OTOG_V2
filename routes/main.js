779920cfbffbe0f66f0179da31e4f27a6b23b76a
Karnjj <karnjj@hotmail.com>
2019-06-30 01:51:27 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -3,14 +3,20 @@ var router = express.Router();
 var mysql = require('mysql');
 var con = mysql.createConnection(global.gConfig.mysql);
 con.connect();
-
+var total_pass = 0;
+var total_nopass = 0;
 function convert(pass) {
   var arr = new Array(5000);
   arr.fill(0);
   pass.forEach(function(e) {
     if(arr[e.prob_id] == 0) {
-      if(e.score == 100) arr[e.prob_id] = 2;
-      else arr[e.prob_id] = 1;
+      if(e.score == 100) {
+        arr[e.prob_id] = 2;
+        total_pass++;
+      }else {
+        arr[e.prob_id] = 1;
+        total_nopass++;
+      }
     }
   });
   return arr;
@@ -18,6 +24,8 @@ function convert(pass) {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  total_pass = 0;
+  total_nopass = 0;
   var pass = [];
   if(req.session.is_login == 1) {
     var sql = "SELECT * FROM Result WHERE user_id = "+req.session.name_id+" ORDER BY time desc";
@@ -34,12 +42,12 @@ router.get('/', function(req, res, next) {
       title: 'main',
       showname: req.session.name_user,
       is_login: req.session.is_login,
-      allprob: result.length,
-      passprob: 0,
-      notpassed: 0,
-      nosub: 0,
-      problems : result,
       pass : convert(pass),
+      allprob: result.length,
+      passprob: total_pass,
+      notpassed: total_nopass,
+      nosub: result.length-(total_pass+total_nopass),
+      problems : result,
     });
     //console.log(Problems);
   });
