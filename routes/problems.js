4a0affbfb14c06e00f66576d5d6a092d338cfd19
Karnjj <karnjj@hotmail.com>
2019-06-30 02:22:46 +0700

    Update problems.js

--- routes/problems.js
+++ routes/problems.js
@@ -39,7 +39,7 @@ router.get('/prob_table', function(req, res, next) {
     });
   }
   //console.log(pass);
-	if(req.session.is_admin == 0) var sql = "SELECT * FROM Problem WHERE ORDER BY id_Prob desc";
+	if(req.session.is_admin == 0) var sql = "SELECT * FROM Problem ORDER BY id_Prob desc";
   else var sql = "SELECT * FROM Problem WHERE state = 1 ORDER BY id_Prob desc";
 	con.query(sql, function (err, rows) {
     	if (err) throw err;
