fb4e6cadea0a76c632f3f0b4c4f1821314a1cb85
Karnjj <karnjj@hotmail.com>
2019-06-29 20:18:30 +0700

    karnjj

--- routes/problems.js
+++ routes/problems.js
@@ -16,9 +16,27 @@ router.get('/',function(req, res, next) {
     is_login : req.session.is_login,
   });
 });
-
+function convert(pass) {
+  var arr = new Array(5000);
+  arr.fill(0);
+  pass.forEach(function(e) {
+    if(e.score == 100) arr[e.prob_id] = 2;
+    else arr[e.prob_id] = 1;
+  });
+  return arr;
+}
 router.get('/prob_table', function(req, res, next) {
 	//res.render("problems.html", {title: 'Problems',});
+  var pass = [];
+  if(req.session.is_login == 1) {
+    var sql = "SELECT prob_id, score, MAX(time) FROM Result WHERE user_id = "+req.session.name_id+" GROUP BY prob_id";
+    console.log(sql);
+    con.query(sql, function (err, rows) {
+      //console.log(rows);
+      pass = rows;
+    });
+  }
+  //console.log(pass);
 	var sql = "SELECT * FROM Problem WHERE state = 1 ORDER BY id_Prob desc";
 	con.query(sql, function (err, rows) {
     	if (err) throw err;
@@ -26,6 +44,7 @@ router.get('/prob_table', function(req, res, next) {
 		res.render("problems/prob_table.html", {
 			title: 'Problems',
 			problems : rows,
+      pass : convert(pass),
       is_login : req.session.is_login,
 		});
 	});
