9814ca9108b834fb32061775e3e50b61b7dd60a1
Karnjj <karnjj@hotmail.com>
2019-07-03 11:36:30 +0700

    Update submission.js

--- routes/submission.js
+++ routes/submission.js
@@ -7,19 +7,11 @@ module.exports = function(io) {
 	/* GET home page. */
 	router.get('/', function(req, res, next) {
 		//res.render("problems.html", {title: 'Problems',});
-		var sql = "SELECT Result.idResult,Problem.name,User.sname,Result.result,Result.score,Result.timeuse,User.rating,Result.user_id,Result.status FROM Result "
-	            +"INNER JOIN Problem ON Result.prob_id=Problem.id_Prob "
-	            +"INNER JOIN User ON Result.user_id=User.idUser ORDER BY Result.time desc";
-		con.query(sql, function (err, rows) {
-	    	if (err) throw err;
-	    	//console.log(rows);
 			res.render("problems/submission.html", {
 				title: 'Problems',
-				submission : rows,
 				is_login: req.session.is_login,
 				id_user: req.session.name_id,
 			});
-		});
 	});
 	/*
 	io.on('connection',function(client){
