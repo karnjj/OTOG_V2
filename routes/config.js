26cf7299c3c5d1e1c0cf66ba3ab78be6e2529c7c
Karnjj <karnjj@hotmail.com>
2019-07-09 14:23:57 +0700

    karnjj
    update config

--- routes/config.js
+++ routes/config.js
@@ -2,15 +2,24 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 var con = mysql.createConnection(global.gConfig.mysql);
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-	//res.render("problems.html", {title: 'Problems',});
-	var sql = "SELECT * FROM Problem";
+  res.render("config/config.html", {
+    title: 'config',
+  });
+});
+router.get('/contest', function(req, res, next) {
+  res.render("config/contest.html", {
+    title: 'config',
+  });
+});
+router.get('/task', function(req, res, next) {
+	var sql = "SELECT * FROM Problem ORDER BY id_Prob desc";
 	con.query(sql, function (err, rows) {
     	if (err) throw err;
     	//console.log(rows);
-		res.render("config/config.html", {
-			title: 'config',
+		res.render("config/task.html", {
+			title: 'task',
 			problems : rows,
 		});
 	});
