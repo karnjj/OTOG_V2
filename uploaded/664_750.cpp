f9fc920d2515a0e3d42cae80fe5f6ed6120e7aec
Karnjj <karnjj@hotmail.com>
2019-10-02 15:12:05 +0700

    update

--- uploaded/664_750.cpp
+++ uploaded/664_750.cpp
@@ -1,37 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int mxn = 1e4 + 10;
-vector<int> adj[mxn];
+const int maxn = 1e6+10;
+int arr[maxn];
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  int n;
-  int start = -1;
-  cin >> n;
-  for(int i = 0; i < n-1; i++) {
-    int a,b;
-    cin >> a >> b;
-    adj[a].push_back(b);
-    adj[b].push_back(a);
+  int i=0;
+  while(cin >> arr[i++]) {
   }
-  for(int i = 1; i <= n; i++) {
-    if(adj[i].size() == 1) {
-      start = i;
-      break;
+  int maxx = 0;
+  int sum = 0;
+  for(int j = i-1; j >=0; j--) {
+    if(arr[j] > maxx) {
+      maxx = arr[j];
+      sum++;
     }
   }
-  int prev = start,now = start,cnt = 1;
-  while(cnt != (n/2+1)) {
-    //cout << "\n" << now << " " << prev;
-    for(auto e : adj[now]) {
-      //cout << "list :" << e << " ";
-      if(e != prev) {
-        prev = now;
-        now = e;
-        break;
-     }
-    }
-    cnt++;
-  }
-  cout << now;
+  cout << sum;
 }
