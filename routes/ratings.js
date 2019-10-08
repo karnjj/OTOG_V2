6293fd7cd62e8fc8bd5baf1666dda68e77fb5292
Karnjj <karnjj@hotmail.com>
2019-10-08 23:44:50 +0700

    Update ratings.js

--- routes/ratings.js
+++ routes/ratings.js
@@ -4,10 +4,9 @@ var mysql = require('mysql');
 var con = mysql.createConnection(global.gConfig.mysql);
 /* GET home page. */
 router.get('/rating_table', function(req, res, next) {
-	var sql = "SELECT sname,rating FROM User ORDER BY rating desc";
-	/*var sql = "SELECT sname,rating FROM User inner join Result as R on User.idUser = R.user_id "+
+	//var sql = "SELECT sname,rating FROM User ORDER BY rating desc";
+	var sql = "SELECT sname,rating FROM User inner join Result as R on User.idUser = R.user_id "+
 			"where contestmode is not null and state = 1 group by sname order by rating desc"
-			*/
 	con.query(sql, function (err, rows) {
     	if (err) throw err;
     	//console.log(rows);
