de82e3f6ddf045a462418ff1cfd5104c673b74b2
Karnjj <karnjj@hotmail.com>
2019-10-08 19:41:20 +0700

    Update cal_rate.py

--- cal_rate.py
+++ cal_rate.py
@@ -36,17 +36,24 @@ mean = numpy.mean(arr)
 zs = []
 rating = []
 for e in arr :
-    zs.append((e-mean)/std)
+	zs.append((e-mean)/std)
 for idx in range(0,len(zs)) :
-    Pa = 1 - norm.cdf(zs[idx])
-    Pb = (rank[idx]-1)/(len(rank)-1)
-    dP = Pa - Pb
-    if dP >= 0:
-        rating.append((dP*100)+25)
-    else :
-        rating.append((dP*100)-25)
+	Pa = 1 - norm.cdf(zs[idx])
+	Pb = (rank[idx]-1)/(len(rank)-1)
+	dP = Pa - Pb
+	if dP >= 0:
+		rating.append(int(round((dP*100)+25)))
+	else :
+		rating.append(int(round((dP*100)-25)));
+	arr[idx] += rating[idx]
+for idx in range(len(user)) :
+	sql = "update User set rating = %s where idUser = %s"
+	val = (arr[idx],user[idx])
+	mycursor.execute(sql, val)
+mydb.commit()
 print("SD : ",std)
 print("MEAN : ",mean)
 print("rank : ",rank)
 print("z-score : ",zs)
 print("rating : ",rating)
+print("update : ",arr)
