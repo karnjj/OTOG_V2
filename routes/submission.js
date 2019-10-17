65c8ad04f9c0409bc605790183f43e5b6dd318d8
Karnjj <karnjj@hotmail.com>
2019-10-18 00:52:21 +0700

    update lastest sub

--- routes/submission.js
+++ routes/submission.js
@@ -1,18 +1,28 @@
 var express = require('express');
 var router = express.Router();
-var fileUpload = require('express-fileupload');
 var mysql = require('mysql');
 var con = mysql.createConnection(global.gConfig.mysql);
 module.exports = function(io) {
-	/* GET home page. */
-	router.get('/', function(req, res, next) {
-		//res.render("problems.html", {title: 'Problems',});
-			res.render("problems/submission.html", {
-				title: 'Problems',
-				is_login: req.session.is_login,
-				id_user: req.session.name_id,
-				is_admin: req.session.is_admin,
-			});
+	router.get('/', async function(req, res, next) {
+		var lastest = null;
+		var name = "";
+		var sql = "select name,id_Prob from ( select max(idResult) as lastest from Result where user_id = ?) "+
+			"as X inner join Result on Result.idResult = X.lastest inner join Problem on prob_id = Problem.id_Prob"
+		if(req.session.name_id != undefined) {
+			lastest = await new Promise((resolve, reject) => con.query(sql,[req.session.name_id], function(err,result){
+				if (err) reject(err)
+				else resolve(result[0]);
+			}));
+			name = lastest.name;
+		}
+		res.render("problems/submission.html", {
+			title: 'Problems',
+			is_login: req.session.is_login,
+			id_user: req.session.name_id,
+			is_admin: req.session.is_admin,
+			lastest : lastest,
+			name: name
+		});
 	});
 	return router;
 };
