c8179df336aaef4b041b09929fa5cbd19806a9dd
Karnjj <karnjj@hotmail.com>
2019-11-19 20:02:56 +0700

    fix auto restart grader

--- routes/restart.js
+++ routes/restart.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const ChildProcess = require('child_process');
-let runSh = new Promise(function(success, nosuccess) {
-    ChildProcess.exec('~/OTOG_V2/./restart.sh',
-    function (error, stdout, stderr) {
-        console.log(error,stdout,stderr);
-        if (error !== null) {
-        nosuccess('exec error: ' + error + '\nstderr : '+stderr);
-        }else success('stdout: ' + stdout);
-    });
-})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    let runSh = new Promise(function(success, nosuccess) {
+        ChildProcess.exec('~/OTOG_V2/./restart.sh',
+        function (error, stdout, stderr) {
+            console.log(error,stdout,stderr);
+            if (error !== null) {
+                nosuccess('exec error: ' + error + '\nstderr : '+stderr);
+            }else success('stdout: ' + stdout);
+        });
+    })
     runSh.then(function(result) {
         res.end(result.toString());
     }).catch(function(result){
